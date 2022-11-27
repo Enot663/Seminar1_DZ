8aae0f21062e5b92e19ac5fb7ce255ff6e859972
Andrey <andrejsatykov@yandex.ru>
2022-11-28 01:37:42 +0300

    Task 002

---
+++ Task_002/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.Write("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число C: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+
+if (numberC > max)
+{
+    max = numberC;
+}
+
+Console.WriteLine($"Максимальное из трех чисел {max}.");
